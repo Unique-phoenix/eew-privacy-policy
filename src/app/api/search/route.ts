49bd1fe47dadffcbbe7babfd2b5623002675b57e
suzushi-tw <ianhuang2023a@gmail.com>
2025-03-30 23:22:15 +0200

    follow static export

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,6 +1,7 @@
 import { source } from '@/lib/source';
 import { createFromSource } from 'fumadocs-core/search/server';
-
-export const dynamic = 'force-static';
-
-export const { GET } = createFromSource(source);
+ 
+// it should be cached forever
+export const revalidate = false;
+ 
+export const { staticGET: GET } = createFromSource(source);
